0ee43a96169383048d49847a5514da819916d5c9
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2024-12-01 13:40:44 -0800

    fix(shell): executable test with correct path

--- .shellrc.d/20-environment/editor.sh
+++ .shellrc.d/20-environment/editor.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ## Set default editor in order of preference
-if [ -x nvim ]; then
+if [ -x "$(command -v nvim)" ]; then
   export EDITOR="/usr/bin/nvim"
-elif [ -x vim ]; then
+elif [ -x "$(command -v vim)" ]; then
   export EDITOR="/usr/bin/vim"
-elif [ -x nano ]; then
-  export EDITOR="/usr/bin/nano"
-elif [ -x vi ]; then
+elif [ -x "$(command -v vi)" ]; then
   export EDITOR="/usr/bin/vi"
+elif [ -x "$(command -v nano)" ]; then
+  export EDITOR="/usr/bin/nano"
 fi
