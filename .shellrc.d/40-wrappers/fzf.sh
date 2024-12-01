09b30fc5b72f63027304a31c55db75a228dbd7c3
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2024-12-01 13:50:01 -0800

    fix(shell): use correct path in dir check

--- .shellrc.d/40-wrappers/fzf.sh
+++ .shellrc.d/40-wrappers/fzf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -d ~/.shellrc.d.d/40-wrappers/fzf ]; then
+if [ -d ~/.shellrc.d/40-wrappers/fzf ]; then
   for rc in ~/.shellrc.d/40-wrappers/fzf/*.sh; do
     if [ -f "$rc" ]; then
       . "$rc"
