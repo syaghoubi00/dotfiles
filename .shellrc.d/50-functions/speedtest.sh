c22952be72d3f853fdac598439cff0fd5186723d
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2024-12-01 14:18:20 -0800

    fix(shell): use fixed api endpoint to fetch iperf servers

--- .shellrc.d/50-functions/speedtest.sh
+++ .shellrc.d/50-functions/speedtest.sh
@@ -5,17 +5,13 @@
 if command -v iperf3 >/dev/null 2>&1; then
   ## Parse North America server list, selecting a random server
   get_iperf_server() {
-    ## TODO: Find constant API endpoint to pull list from
-    server_list="https://db.iperf3serverlist.net/api/v1/db/public/shared-view/956847fb-43b7-4404-b048-440518fbc763/rows/export/csv"
+    SERVER_LIST="https://export.iperf3serverlist.net/json.php"
 
-    if curl --output /dev/null --silent --head --fail "$server_list"; then
-      curl --silent "$server_list" |
-        tail -n +2 |
-        cut -d , -f 1 |
-        sort -R |
-        head -n 1
+    if curl --output /dev/null --silent --head --fail "$SERVER_LIST"; then
+      curl --silent "$SERVER_LIST" | jq -r '.[] | select( .COUNTRY | contains("US")) | .IP_HOST' | shuf -n1 | cut -d ' ' -f 2-5
     else
-      echo "Unable to fetch iperf server - the source URL may need to be updated"
+      echo "Unable to fetch iperf server"
+      return 1
     fi
   }
 
