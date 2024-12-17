fa5f8b1bba854173bea63c5c9493da074ab136d3
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2024-12-17 14:42:08 -0800

    feat(shell-aliases): add git alias to stage all tracked files

--- .shellrc.d/30-aliases/git.sh
+++ .shellrc.d/30-aliases/git.sh
@@ -4,5 +4,6 @@ if command -v git >/dev/null 2>&1; then
   alias gs="git status"
   alias gd="git diff"
   alias ga="git add"
+  alias gau="git add -u" # stages all modified and deleted tracked files.
   alias gc="git commit"
 fi
