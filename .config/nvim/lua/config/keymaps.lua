80272cf664bf4b87f612d191b9c84c2966d15f5d
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2024-12-21 09:40:01 -0800

    feat(nvim): remap annoying f1 help key to esc

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,13 @@ vim.keymap.set("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" 
 vim.keymap.set("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
 vim.keymap.set("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move Block Down" })
 vim.keymap.set("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move Block Up" })
+
+--[[
+Disable opening the neovim help page with F1,
+am always hitting F1 instead of escape (right next to eachother on kb),
+was going to map to <Nop>, but might as well map to <Esc>,
+since that was probably what I was trying to do anyway.
+]]
+vim.api.nvim_set_keymap("n", "<F1>", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<F1>", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<F1>", "<Esc>", { noremap = true, silent = true })
