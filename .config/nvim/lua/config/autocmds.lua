b88e49d2e442849ae8732b927c0ea1c64aab29ab
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2024-12-20 17:33:10 -0800

    feat(nvim): add ability to sort yaml keys

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- YAML key sorting
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "yaml",
+  callback = function()
+    vim.api.nvim_create_user_command("SortYamlKeys", function()
+      vim.cmd('%!yq --prettyPrint "sort_keys(..)" -')
+    end, {})
+
+    vim.keymap.set("n", "<leader>cs", "<cmd>SortYamlKeys<CR>", { desc = "Sort YAML Keys", buffer = true })
+  end,
+})
