6f006e9c0edb58b9095e54e406534e21689fc436
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2025-02-16 17:05:11 -0800

    fix(shell.env.man): use updated manpager variable

--- .shellrc.d/20-environment/man.sh
+++ .shellrc.d/20-environment/man.sh
@@ -2,6 +2,5 @@
 
 ## Use `bat` to print man pages if `bat` is installed
 if command -v bat >/dev/null 2>&1; then
-  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-  export MANROFFOPT="-c"
+  export MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
 fi
